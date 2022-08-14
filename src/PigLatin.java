d8d006d9ac38446324caa3a39565a45f46509dc4
Maksym Dominichenko <max@dominichenko.com>
2022-08-14 00:27:40 -0400

    Formatting sources

--- src/PigLatin.java
+++ src/PigLatin.java
@@ -12,38 +12,48 @@ import java.util.regex.Pattern;
  * Pig Latin is a language game or argot in which English words are altered, usually by adding a
  * fabricated suffix or by moving the onset or initial consonant or consonant cluster of a word to
  * the end of the word and adding a vocalic syllable to create such a suffix.
+ *
  * <p>Write a simplified solution that implement the following rules:
- * <p>For words that begin with consonant sounds, all letters before the initial vowel are placed
- * at the end of the word sequence. Then, "ay" is added, as in the following examples:
+ *
+ * <p>For words that begin with consonant sounds, all letters before the initial vowel are placed at
+ * the end of the word sequence. Then, "ay" is added, as in the following examples:
+ *
  * <ul>
- *   <li>"pig" = "ig-pay"</li>
- *   <li>"latin" = "atin-lay"</li>
- *   <li>"banana" = "anana-bay"</li>
- *   <li>"will" = "ill-way"</li>
+ *   <li>"pig" = "ig-pay"
+ *   <li>"latin" = "atin-lay"
+ *   <li>"banana" = "anana-bay"
+ *   <li>"will" = "ill-way"
  * </ul>
- * <p>When words begin with consonant clusters, the whole sound is added to the end when speaking or writing.
+ *
+ * <p>When words begin with consonant clusters, the whole sound is added to the end when speaking or
+ * writing.
+ *
  * <ul>
- *   <li>"smile" = "ile-smay"</li>
- *   <li>"string" = "ing-stray"</li>
- *   <li>"stupid" = "upid-stay"</li>
+ *   <li>"smile" = "ile-smay"
+ *   <li>"string" = "ing-stray"
+ *   <li>"stupid" = "upid-stay"
  * </ul>
+ *
  * <p>For words that begin with vowel sounds, just only a syllable 'way' is appended to the end:
+ *
  * <ul>
- *   <li>"eat" = "eat-way"</li>
- *   <li>"omelet" = "omelet-way"</li>
- *   <li>"are" = "are-way"</li>
+ *   <li>"eat" = "eat-way"
+ *   <li>"omelet" = "omelet-way"
+ *   <li>"are" = "are-way"
  * </ul>
- * <p>Add a "-" separator between modified word and suffix for convenience.
- * Letter cases should be preserved, and all the punctuation should be untouched.
- * So, e.g. the phrase:<br/>
- * <code>I've been wondering - what is the answer?!?!? How will we know it's correct?</code>
- * <br/>should be transformed to:<br/>
- * <code>I've-way een-bay ondering-way - at-whay is-way e-thay answer-way?!?!? ow-Hay ill-way e-way ow-knay it's-way orrect-cay?</code>
+ *
+ * <p>Add a "-" separator between modified word and suffix for convenience. Letter cases should be
+ * preserved, and all the punctuation should be untouched. So, e.g. the phrase:<br>
+ * <code>I've been wondering - what is the answer?!?!? How will we know it's correct?</code> <br>
+ * should be transformed to:<br>
+ * <code>
+ * I've-way een-bay ondering-way - at-whay is-way e-thay answer-way?!?!? ow-Hay ill-way e-way ow-knay it's-way orrect-cay?
+ * </code>
  */
 public class PigLatin {
 
-  private static final Pattern consonants = Pattern.compile(
-      "^([bcdfghjklmnpqrstvwxzBCDFGHJKLMNPQRSTVWXZ']+)(.*)$");
+  private static final Pattern consonants =
+      Pattern.compile("^([bcdfghjklmnpqrstvwxzBCDFGHJKLMNPQRSTVWXZ']+)(.*)$");
   private static final Pattern vowels = Pattern.compile("^[aeiouyAEIOUY']+.*$");
 
   public static String translate(String phrase) {
@@ -74,7 +84,8 @@ public class PigLatin {
   public static void main(String[] args) {
     final String phrase =
         Optional.of(args)
-            .filter(a -> a.length > 0).map(a -> a[0])
+            .filter(a -> a.length > 0)
+            .map(a -> a[0])
             .orElse("I've been wondering - what is the answer?!?!? How will we know it's correct?");
     System.out.println("phrase: " + phrase);
     System.out.println("result: " + translate(phrase));
--- src/TargetCandidates.java
+++ src/TargetCandidates.java
@@ -10,8 +10,10 @@ import java.util.stream.Stream;
 /**
  * For given list of integer values named "candidates", write a solution to generate all possible
  * unique combinations of elements that have a target sum.
- * <p>E.g. for given candidates: [10, 1, 2, 7, 1, 6, 2, 5] and target sum 8, the result should
- * be like:
+ *
+ * <p>E.g. for given candidates: [10, 1, 2, 7, 1, 6, 2, 5] and target sum 8, the result should be
+ * like:
+ *
  * <pre><code>
  * [[1, 2, 5], [2, 1, 5], [2, 6], [1, 1, 6], [1, 7], [7, 1], [6, 2]]
  * </code></pre>
@@ -29,22 +31,22 @@ public class TargetCandidates {
   public Set<List<Integer>> get() {
     final Set<List<Integer>> result = ConcurrentHashMap.newKeySet();
     generateIndices(
-        List.of(),
-        idx ->
-            Optional.of(idx.stream().map(candidates::get).toList())
-                .filter(v -> v.stream().mapToInt(Integer::intValue).sum() == target)
-                .ifPresent(result::add))
+            List.of(),
+            idx ->
+                Optional.of(idx.stream().map(candidates::get).toList())
+                    .filter(v -> v.stream().mapToInt(Integer::intValue).sum() == target)
+                    .ifPresent(result::add))
         .forEach(v -> {});
     return result;
   }
 
   private Stream<List<Integer>> generateIndices(
-      List<Integer> chunk, Consumer<List<Integer>> idxConsumer) {
-    return Optional.of(chunk.isEmpty() ? 0 : chunk.get(chunk.size() - 1) + 1)
+      List<Integer> idx, Consumer<List<Integer>> idxConsumer) {
+    return Optional.of(idx.isEmpty() ? 0 : idx.get(idx.size() - 1) + 1)
         .map(
             start ->
                 IntStream.range(start, candidates.size())
-                    .mapToObj(i -> Stream.concat(chunk.stream(), Stream.of(i)).toList())
+                    .mapToObj(i -> Stream.concat(idx.stream(), Stream.of(i)).toList())
                     .parallel()
                     .peek(idxConsumer)
                     .flatMap(v -> generateIndices(v, idxConsumer)))
@@ -58,10 +60,7 @@ public class TargetCandidates {
             .map(a -> Arrays.stream(a[0].split("\\s*,\\s*")).map(Integer::parseInt).toList())
             .orElseGet(() -> List.of(10, 1, 2, 7, 1, 6, 2, 5));
     final int target =
-        Optional.of(args)
-            .filter(a -> a.length > 1)
-            .map(a -> Integer.parseInt(a[1]))
-            .orElse(8);
+        Optional.of(args).filter(a -> a.length > 1).map(a -> Integer.parseInt(a[1])).orElse(8);
     System.out.println("candidates: " + candidates);
     System.out.println("target: " + target);
     System.out.println("result: " + new TargetCandidates(candidates, target).get());
